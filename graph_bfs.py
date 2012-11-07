0290972289ff4cfc95d5259d7cd9dbec8e3e6651
Jaconey <jaconey@gmail.com>
2012-11-07 18:50:32 +0800

    add class Graph for BFS

--- graph_bfs.py
+++ graph_bfs.py
@@ -36,6 +36,31 @@ def BFS(g, start):
 				color[child] = G
 				q.append(child)
 		color[node] = B
+		print node
+
+class Graph():
+	def __init__(self, g):
+		self._color = dict()
+		self._g = g
+		self.Clear()
+
+	def Clear(self):
+		for k in self._g.iterkeys():
+			self._color[k] = W
+
+	def BFS(self, start):
+		if self._color[start] == W:
+			self._color[start] = G
+		else:
+			return
+
+		self._color[start] = B
+		print start
+		for child in self._g[start]:
+			self.BFS(child)
 
 #BFS(graph, 'A')
 BFS(graph2, 's')
+print "-----"
+g = Graph(graph2)
+g.BFS('s')
